17b4181573187f9112c491cf1b7f5cc2ee097a67
pedropenhavn <pedro.afonso@vilanova.com.br>
2025-09-11 19:32:00 -0300

    COnfig logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -71,6 +71,6 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
         
-        return redirect('http://localhost:5000/login');
+        return redirect('https://app.logiez.com.br/login');
     }
 }
